c094079ab9d69fb0a549a2d017a3d0f32cc4994b
ndeme yvan <yvan.ndeme@papersoft.ng>
2021-04-06 14:42:06 -0600

    change

--- src/components/MyWorks.vue
+++ src/components/MyWorks.vue
@@ -24,25 +24,18 @@
           </div>
         </div>
         <div class="col-md-6 project_images">
-          <div class="image_item1">
-          
-          </div>
-          <div class="image_item2">
-            
-          </div>
-          <div class="image_item3">
-           
-          </div>
-          <div class="image_item4">
-           
-          </div>
-          <div class="image_item2">
-          </div>
-          <div class="image_item4">
-          </div>
+          <div class="block image_item2"></div>
+          <div class="block image_item1"></div>
+          <div class="image_item3"></div>
+          <div class="image_item4"></div>
+          <div class="image_item5"></div>
+          <div class="image_item6"></div>
         </div>
       </div>
     </div>
+
+
+    
   </div>
 </template>
 
@@ -91,6 +84,7 @@ export default {
 .project_summary {
   display: flex;
   justify-content: center;
+  align-items: center
 }
 
 .contact-me {
@@ -117,13 +111,12 @@ export default {
 }
 .project_images {
   display: grid;
-  grid-template-columns: 25% 25% 25%;
+  grid-template-columns: 10fr 10fr 10fr;
 }
-
 
 .image_item1 {
   background-image: url("../assets/img/login_ui.jpg");
-  background-size: cover; 
+  background-size: cover;
   height: 250px;
   background-color: fuchsia;
   cursor: pointer;
@@ -131,37 +124,75 @@ export default {
 
 .image_item2 {
   background-image: url("../assets/img/efram_home.jpg");
-  background-size: cover; 
+  background-size: cover;
   height: 250px;
   background-color: green;
-   cursor: pointer;
+  cursor: pointer;
 }
 .image_item3 {
   background-image: url("../assets/img/portfolioApp.jpg");
-   background-size: cover; 
-   height: 250px;
+  background-size: cover;
+  height: 250px;
   background-color: firebrick;
-   cursor: pointer;
+  cursor: pointer;
 }
 .image_item4 {
   background-image: url("../assets/img/floraison.jpg");
-  background-size: cover; 
-   height: 250px;
+  background-size: cover;
+  height: 250px;
   background-color: blue;
-   cursor: pointer;
+  cursor: pointer;
 }
 .image_item5 {
-  background-image: url("../assets/img/floraison.jpg");
-  background-size: cover; 
-   height: 250px;
+  background-image: url("../assets/img/b_commerc.jpg");
+  background-size: cover;
+  height: 250px;
   background-color: blue;
-   cursor: pointer;
+  cursor: pointer;
 }
 .image_item6 {
-  background-image: url("../assets/img/floraison.jpg");
-  background-size: cover; 
-   height: 250px;
+  background-image: url("../assets/img/activa_showroom.png");
+  background-size: cover;
+  height: 250px;
   background-color: blue;
-   cursor: pointer;
+  cursor: pointer;
+}
+
+.block{
+  -webkit-animation:scaledown 1s linear;
+  -moz-animation:scaledown 1s linear;
+  animation:scaledown 1s linear;
+  transform-origin:50% 50%;
+  animation-fill-mode: forwards;
+}
+
+.block:hover{
+   z-index:100;
+  -webkit-animation:scale 1s linear;
+  -moz-animation:scale 1s linear;
+  animation:scale 1s linear;
+  transform-origin:50% 50%;
+  animation-fill-mode: forwards;
+}
+
+@keyframes scale{
+  0%{
+    transform:scale(1.0);
+  }
+  100%{
+    transform:scale(1.1);
+    -webkit-box-shadow: 10px 10px 60px 10px rgba(0,0,0,0.1);
+-moz-box-shadow: 10px 10px 60px 10px rgba(0,0,0,0.1);
+box-shadow: 10px 10px 60px 10px rgba(0,0,0,0.1);
+  }
+}
+
+@keyframes scaledown{
+  0%{
+    transform:scale(1.1);
+  }
+   100%{
+    transform:scale(1.0);
+  }
 }
 </style>
